1f8549c8931f91a7d2a2c4ab54a2a7104d587644
lithrein <lithrein.site@gmail.com>
2017-12-10 16:27:28 +0100

    change of representation for laws

--- src/law.rb
+++ src/law.rb
@@ -1,7 +1,41 @@
-class Law
-  attr_accessor :name
+class Model
+  attr_reader :attributes
 
-  def eval
-    raise NotImplementedError
+  def initialize
+    @attributes = {}
+  end
+  
+  def method_missing(name, args, &block)
+    @attributes[name] = args
+  end
+end
+
+module Law
+  @klasses = {}
+  
+  def self.define(name, &block)
+    model = Model.new
+    model.instance_eval(&block)
+    builder = Class.new do
+      @@attributes = []
+
+      attrs = model.attributes
+      attrs.each do |attr, val|
+        if val.is_a?(Proc) then
+          define_method(attr, val)
+        else
+          attr_accessor val
+          @@attributes << val
+        end
+      end
+
+      def initialize(*args)
+        @@attributes[0..args.size-1].zip(args) do |attr, val|
+          send "#{attr}=", val
+        end
+      end
+    end
+    klass = Object.const_set name, builder
+    @klasses[name] = klass
   end
 end
--- src/test.rb
+++ src/test.rb
@@ -1,12 +1,19 @@
 require './app'
 
+Law.define :RandomLaw do
+  type :behavior
+  parameters :range
+  run -> (time, neighbors, sensor) { rand(@range) }
+end
+
 Simulation.define do
-  name "Test App"
+  name "Test App"  
   
   create Pool do
     name "Pool 1"
     captors Array.new(10){Sensor.new}
     law RandomLaw.new 1..2
+    options :chaos_monkey
   end
   
   create Computer do
@@ -15,6 +22,6 @@ Simulation.define do
     user "test"
     password "test"
   end
-
+  
   monitor "Pool 1", with: "Computer 1"
 end
